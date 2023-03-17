f8cca48f13f3aae3d212378e3ab1c7f837ab1c7e
akash <pratap_the_knight@yahoo.com>
2023-03-17 15:52:39 -0400

    first commit

---
+++ wdio.conf.ts
@@ -0,0 +1,71 @@
+import type { Options } from "@wdio/types";
+import allure from "@wdio/allure-reporter";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config: Options.Testrunner = {
+  //
+  // ====================
+  // Runner Configuration
+  // ====================
+  // WebdriverIO supports running e2e tests as well as unit and component tests.
+  runner: "local",
+  autoCompileOpts: {
+    tsNodeOpts: {
+      project: "./tsconfig.json",
+    },
+  },
+
+  specs: ["./test/features/**/*.feature"],
+  exclude: [],
+  maxInstances: 10,
+  capabilities: [
+    {
+      maxInstances: 5,
+      browserName: "chrome",
+      acceptInsecureCerts: true,
+    },
+  ],
+
+  logLevel: "info",
+
+  bail: 0,
+  baseUrl: "http://localhost",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ["chromedriver"],
+  framework: "cucumber",
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        useCucumberStepReporter: true,
+      },
+    ],
+  ],
+
+  cucumberOpts: {
+    require: ["./test/features/step-definations/*.ts"],
+    backtrace: false,
+    requireModule: [],
+    dryRun: false,
+    failFast: false,
+    snippets: true,
+    source: true,
+    strict: false,
+    tagExpression: "@demo",
+    timeout: 600000,
+    ignoreUndefinedDefinitions: false,
+  },
+
+  afterStep: async function (step, scenario, result, context) {
+    // Take screenshot if failed
+    if (!result.passed) {
+      await browser.takeScreenshot();
+    }
+  },
+};
